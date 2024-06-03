7d6d836a62bf35a8debf03b04d956e8e2d32adba
Nick <53293116+nicholasvo@users.noreply.github.com>
2024-06-03 01:48:40 -0700

    small transcription fixes

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from chain import *
 
 vocab = ["move", "turn", "left", "pick", "beeper", "put", "underscore", "for", "range", 
          "front", "clear", "present", "bag", "beepers", "blocked", "while", 
-         "indent", "return", "unindent", "backspace"]
+         "indent", "return", "unindent", "backspace", "tab"]
 
 class ASRCodingModel():
     def __init__(self, model_type = "base.en"):
